967a449a1b1baaa1c466c6e726aa5545ddf436e0
Michael <mstephenp@protonmail.com>
2021-05-02 07:26:21 -0500

    adding avatar uploads

--- lib/instagram_clone_web/live/user_live/settings.ex
+++ lib/instagram_clone_web/live/user_live/settings.ex
@@ -4,6 +4,9 @@ defmodule InstagramCloneWeb.UserLive.Settings do
 
   alias InstagramClone.Accounts
   alias InstagramClone.Accounts.User
+  alias InstagramCloneWeb.Uploaders.Avatar
+
+  @extension_whitelist ~w(.jpg .jpeg .png)
 
   @impl true
   def mount(_params, session, socket) do
@@ -13,7 +16,12 @@ defmodule InstagramCloneWeb.UserLive.Settings do
     {:ok,
       socket
       |> assign(changeset: changeset)
-      |> assign(page_title: "Edit Profile")}
+      |> assign(page_title: "Edit Profile")
+      |> allow_upload(:avatar_url,
+      accept: @extension_whitelist,
+      max_file_size: 9_000_000,
+      progress: &handle_progress/3,
+      auto_upload: true)}
   end
 
   @impl true
@@ -29,7 +37,7 @@ defmodule InstagramCloneWeb.UserLive.Settings do
   @impl true
   def handle_event("save", %{"user" => user_params}, socket) do
     case Accounts.update_user(socket.assigns.current_user, user_params) do
-      {:ok, user} ->
+      {:ok, _user} ->
         {:noreply,
           socket
           |> put_flash(:info, "User updated successfully")
@@ -40,4 +48,30 @@ defmodule InstagramCloneWeb.UserLive.Settings do
     end
   end
 
+  @impl true
+  def handle_event("upload_avatar", _params, socket) do
+    {:noreply, socket}
+  end
+
+  def handle_progress(:avatar_url, entry, socket) do
+    if entry.done? do
+      avatar_url = Avatar.get_avatar_url(socket, entry)
+      user_params = %{"avatar_url" => avatar_url}
+
+      case Accounts.update_user(socket.assigns.current_user, user_params) do
+        {:ok, _user} ->
+          Avatar.update(socket, socket.assigns.current_user.avatar_url, entry)
+          current_user = Accounts.get_user!(socket.assigns.current_user.id)
+          {:noreply,
+            socket
+            |> put_flash(:info, "Avatar updated successfully")
+            |> assign(current_user: current_user)}
+
+        {:error, %Ecto.Changeset{} = changeset} ->
+          {:noreply, socket |> assign(changeset: changeset)}
+      end
+    else
+      {:noreply, socket}
+    end
+  end
 end
